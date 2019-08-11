ea2bf2f75ac35da154f824ed3c4a53b4372ab359
Brian Wong <bwong92@gmail.com>
2019-08-11 21:10:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,4 +21,14 @@ def add_item(item, price, quantity = 1)
     @items << item
   end
 end
+
+  def apply_discount
+     if @discount > 0
+       @to_take_off = (price * discount)/100
+       @total -= @to_take_off
+       return "After the discount, the total comes to $#{total}."
+     else
+       return "There is no discount to apply."
+     end
+   end
 end
