a51c7bc8f50a260b77d6f29bc46346e69721765c
Brian Wong <bwong92@gmail.com>
2019-08-11 21:10:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
 
+class CashRegister
+attr_accessor :total, :discount, :price, :items
+
+def initialize(discount = 0)
+  @total = 0
+  @discount = discount
+  @items = []
+end
+
+def add_item(item, price, quantity = 1)
+  @price = price
+  @total += price * quantity
+  if quantity > 1
+    counter = 0
+    while counter < quantity
+      @items << item
+      counter += 1
+    end
+  else
+    @items << item
+  end
+end
